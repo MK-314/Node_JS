86758fd28e781b4e937b7de477d780f32032ddf7
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-02 11:00:00 -0700

    Name var test

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
-var fs = require('fs');
-// var data = fs.writeFileSync('test.txt', 'I\'m very smart!');
-var data = fs.appendFileSync('test.txt', '\nI\'m very smart too!');
+var name = require('./utils.js');
+
+
+console.log(name);
