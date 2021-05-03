5e25d8f8c39c7596ec0909fb5afd85428ba328f9
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-02 22:09:59 -0700

    chalk added for fun

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,6 @@
+var chalk = require('chalk');
 var fs = require('fs');
-const { parse } = require('path');
+const log = console.log;
 var getNotes = function() {
     return "Your notes...";
 };
@@ -17,29 +18,32 @@ var addNote = function(title, body) {
             title: title,
             body: body
         });
-        console.log("New note added");
+        console.log(chalk.green.inverse("New note added"));
         saveNotes(notes);
     } else {
-        console.log("We have the Note with the same Title");
+        console.log(chalk.red.inverse("We have the Note with the same Title"));
     }
 
 };
 var removeNote = function(title) {
     var notes = loadNotes();
-    // var index = -1;
-    // notes.forEach(element => {
-    //     if (element.title == title) {
-    //         index = notes.indexOf(element);
-    //     }
-    // });
-    // if (index > -1) {
-    //     notes.splice(index, 1);
-    //     saveNotes(notes);
-    // }
-    var notesToKeep = notes.filter(function(note) {
-        return note.title !== title;
+    var index = -1;
+    notes.forEach(element => {
+        if (element.title == title) {
+            index = notes.indexOf(element);
+        }
     });
-    saveNotes(notesToKeep);
+    if (index > -1) {
+        notes.splice(index, 1);
+        saveNotes(notes);
+        log(chalk.white.bgGreen('Note removed!'));
+    } else {
+        log(chalk.white.bgRed('No note found!'));
+    }
+    // var notesToKeep = notes.filter(function(note) {
+    //     return note.title !== title;
+    // });
+    // saveNotes(notesToKeep);
 }
 
 var saveNotes = function(notes) {
