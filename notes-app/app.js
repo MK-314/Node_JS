88b79518387f2e907f51a6b1ed4e3ab4cd1c32db
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-02 18:13:02 -0700

    test body and title

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,9 +8,30 @@ var chalk = require('chalk');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of the Note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title + "\n" + 'Body: ' + argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove a node',
     handler: function() {
-        console.log('Adding a new note');
+        console.log('removing a note');
     }
 });
 // add, remove, read, list
-console.log(yargs.argv);
+// console.log(yargs.argv);
+yargs.parse();
