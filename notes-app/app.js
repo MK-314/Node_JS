571ce615ddc4bcc839d6ee7aa8681dd2f4ccf555
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-02 17:24:39 -0700

    yargs functions

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,16 @@
 var validator = require('validator');
+var yargs = require('yargs');
 var chalk = require('chalk');
-console.log(chalk.green.bold('Success!'));
 
-console.log(validator.isEmail("michael.kashkov@gmail.com"));
+// console.log(process.argv);
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+});
+// add, remove, read, list
+console.log(yargs.argv);
