e5ead184ae783a13df5748e79f5c9886d58539bc
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-04 23:57:14 -0700

    api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,10 @@
-const log = console.log;
+var request = require('request');
+var log = console.log;
 
-log('Starting');
+var url = 'http://api.weatherstack.com/current?access_key=8600d40be5dd78dfbd6cabd45b967ab2&query=37.8267,-122.4233&units=f';
 
-setTimeout(() => {
-    log('2 sec timer');
-}, 2000);
-setTimeout(() => {
-    log('0 sec timer');
-}, 0);
-
-log('Stopping');
+request({ url: url, json: true }, (error, response) => {
+    // var dataObject = JSON.parse(response.body);
+    // log(response.body.current);
+    log(response.body.current.weather_descriptions[0] + ", It is currently " + response.body.current.temperature + " It feels like " + response.body.current.feelslike)
+});
