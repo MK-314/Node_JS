14acc540cf7dcc344d5c0347e12d88d4068c9e4f
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-05 23:07:39 -0700

    npm i express

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,18 @@
 var weather_util = require('./utils/weather_util');
 var log = console.log;
 
-weather_util.geocode('Philadelphia', (error, data) => {
-    log(error);
-    log(data);
 
-})
-weather_util.forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+
+
+weather_util.geocode('Vancouver', (error, data) => {
+    if (error) {
+        return log(error)
+    }
+    weather_util.forecast(data.longitude, data.latitude, (error, forecastData) => {
+        if (error) {
+            return log(error)
+        }
+        log(data.location)
+        log(forecastData)
+    })
 })
--- weather-app/utils/weather_util.js
+++ weather-app/utils/weather_util.js
@@ -2,29 +2,29 @@ var request = require('request');
 var log = console.log;
 var geocode = (address, callback) => {
     var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWljaGFlbGthc2hrb3YiLCJhIjoiY2tvYzBnYjB0MDM2dTJwcGQ1OXI3Z2plaiJ9.eefLWmTaqE5wpjYwNg0h8w&limit=1';
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services', undefined);
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search', undefined);
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     });
 }
 var forecast = (la, lo, callback) => {
-    var url = 'http://api.weatherstack.com/current?access_key=8600d40be5dd78dfbd6cabd45b967ab2&query=' + lo + ',' + la + '&units=f';
-    request({ url: url, json: true }, (error, response) => {
+    var url = 'http://api.weatherstack.com/current?access_key=8600d40be5dd78dfbd6cabd45b967ab2&query=' + lo + ',' + la + '&units=m';
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather services', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ", It is currently " + response.body.current.temperature + " It feels like " + response.body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0] + ", It is currently " + body.current.temperature + " It feels like " + body.current.feelslike)
         }
     });
 }
