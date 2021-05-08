d8949091a0d092c713f40ac0979819dad28f15c8
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-08 13:52:43 -0700

    footer and header

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,19 @@
 var path = require('path');
 var express = require('express');
+var hbs = require('hbs');
 log = console.log;
 
 var app = express();
 
 // Define path for Express conf:
 var publicDirPath = path.join(__dirname, '../public');
-var viewsPath = path.join(__dirname, '../templates');
+var viewsPath = path.join(__dirname, '../templates/views');
+var partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location:
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directoty to serve:
 app.use(express.static(publicDirPath));
