36e28778fa978bd184a9546390f2bbe308c49554
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-05-08 13:27:20 -0700

    hbs files vs html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,16 +3,30 @@ var express = require('express');
 log = console.log;
 
 var app = express();
-var publicDirPath = path.join(__dirname, '../public');
 
+// Define path for Express conf:
+var publicDirPath = path.join(__dirname, '../public');
+var viewsPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location:
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directoty to serve:
 app.use(express.static(publicDirPath));
 
 
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: "Sunny",
-        location: "Toronto"
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Mike'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: "Mike"
     });
 })
 
